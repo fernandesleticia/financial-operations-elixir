7ffcf8019ffab4b2e6392099ec20fd79d00d088b
fernandesleticia <leticiafernandes777@gmail.com>
2020-04-04 12:43:37 -0300

    styling home page

--- test/financial_operations_elixir_web/controllers/page_controller_test.exs
+++ test/financial_operations_elixir_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule FinancialOperationsElixirWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Financial Operations Elixir!"
   end
 end
