1ead85f7b4bec7d816eff730a7b38a41e2ce467a
fernandesleticia <leticiafernandes777@gmail.com>
2020-04-04 11:57:11 -0300

    adding navigatin bar

---
+++ test/financial_operations_elixir_web/templates/layout/navigation_test.exs
@@ -0,0 +1,9 @@
+defmodule FinancialOperationsElixirWeb.NavigationTest do
+  use FinancialOperationsElixirWeb.ConnCase
+
+  test "shows a sign in with Google link when not signed in", %{conn: conn} do
+    conn = get conn, "/"
+    
+    assert html_response(conn, 200) =~ "Sign in with Google"
+  end
+end
