799f88c60ac0bf1f670bd0538044e4e8d1d0e14a
fernandesleticia <leticiafernandes777@gmail.com>
2020-04-07 11:30:12 -0300

    fix test for page controller

--- test/financial_operations_elixir_web/controllers/page_controller_test.exs
+++ test/financial_operations_elixir_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule FinancialOperationsElixirWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Financial Operations Elixir!"
+    assert html_response(conn, 200) =~ "Welcome to Financial Operations!"
   end
 end
