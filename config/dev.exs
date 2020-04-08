e9b3c2c64896bdd4dc8b700612e89e9b727776a6
fernandesleticia <leticiafernandes777@gmail.com>
2020-04-08 11:24:44 -0300

    config: removing debug errors trought pages

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :financial_operations_elixir, FinancialOperationsElixir.Repo,
 # with webpack to recompile .js and .css sources.
 config :financial_operations_elixir, FinancialOperationsElixirWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: [
