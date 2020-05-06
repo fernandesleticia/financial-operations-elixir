81432c35a3def978cff1e1ad4cafc2f0172f0ef6
fernandesleticia <leticiafernandes777@gmail.com>
2020-05-06 13:22:29 -0300

    feat: adding payments service

--- lib/financial_operations_elixir_web/controllers/batch_payment_controller.ex
+++ lib/financial_operations_elixir_web/controllers/batch_payment_controller.ex
@@ -3,12 +3,10 @@ defmodule FinancialOperationsElixirWeb.BatchPaymentController do
 
   alias FinancialOperationsElixir.Batches
   alias FinancialOperationsElixir.Batches.BatchPayment
-  alias FinancialOperationsElixir.Payments
-  alias FinancialOperationsElixir.Payments.Payment
   alias FinancialOperationsElixir.Transactions
   alias FinancialOperationsElixir.Transactions.Transaction
   alias FinancialOperationsElixirWeb.Services.Currencies.Exchange
-  alias FinancialOperationsElixirWeb.Services.Utils.GenerateCode
+  alias FinancialOperationsElixirWeb.Services.Payments.Create
 
   action_fallback FinancialOperationsElixirWeb.FallbackController
 
@@ -61,7 +59,7 @@ defmodule FinancialOperationsElixirWeb.BatchPaymentController do
     status = "open"
     batch_payment_params = %{"total_value"=>total_value, "currency_id"=>currency_id, "payer_id"=>payer_id, "payments"=>payments, "status"=>status}
     with {:ok, %BatchPayment{} = batch_payment} <- Batches.create_batch_payment(batch_payment_params) do
-      mount_payments(payments)
+      Create.mount_payments(payments)
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.batch_payment_path(conn, :show, batch_payment))
@@ -87,31 +85,31 @@ defmodule FinancialOperationsElixirWeb.BatchPaymentController do
 
   # payments service
   #Payment struct
-  defmodule Payment do
-    defstruct tracking_code: "", value: 0.0, beneficiary_id: 0, transaction_id: 0, batch_id: 0 
-  end
+  # defmodule Payment do
+  #   defstruct tracking_code: "", value: 0.0, beneficiary_id: 0, transaction_id: 0, batch_id: 0 
+  # end
 
-  defp payments(payments_drafts_json) do
-    Poison.decode!(payments_drafts_json, as: [%Payment{}])
-  end
+  # defp payments(payments_drafts_json) do
+  #   Poison.decode!(payments_drafts_json, as: [%Payment{}])
+  # end
 
-  defp payments_params(payment_draft) do
-    %{"tracking_code"=>payment_draft.tracking_code, "value"=>payment_draft.value, "transaction_id"=>payment_draft.transaction_id, "beneficiary_id"=>payment_draft.beneficiary_id, "batch_id"=>payment_draft.batch_id}
-  end
+  # defp payments_params(payment_draft) do
+  #   %{"tracking_code"=>payment_draft.tracking_code, "value"=>payment_draft.value, "transaction_id"=>payment_draft.transaction_id, "beneficiary_id"=>payment_draft.beneficiary_id, "batch_id"=>payment_draft.batch_id}
+  # end
 
-  defp create_payment(payment_draft) do
-    tracking_code = GenerateCode.generate(10)
-    batch_id = System.unique_integer()
-    transaction_id = System.unique_integer()
-    beneficiary_id = System.unique_integer()
-    payment_draft = %{payment_draft | transaction_id: transaction_id, batch_id: batch_id, tracking_code: tracking_code, beneficiary_id: beneficiary_id}
+  # defp create_payment(payment_draft) do
+  #   tracking_code = GenerateCode.generate(10)
+  #   batch_id = System.unique_integer()
+  #   transaction_id = System.unique_integer()
+  #   beneficiary_id = System.unique_integer()
+  #   payment_draft = %{payment_draft | transaction_id: transaction_id, batch_id: batch_id, tracking_code: tracking_code, beneficiary_id: beneficiary_id}
     
-    with {:ok, %FinancialOperationsElixir.Payments.Payment{} = payment} <- payments_params(payment_draft) |> Payments.create_payment() do
-      create_transaction(payment)
-    end 
-  end 
+  #   with {:ok, %FinancialOperationsElixir.Payments.Payment{} = payment} <- payments_params(payment_draft) |> Payments.create_payment() do
+  #     create_transaction(payment)
+  #   end 
+  # end 
 
-  defp mount_payments(payments_drafts_json) do
-    Enum.each(payments(payments_drafts_json), &create_payment/1)  
-  end
+  # defp mount_payments(payments_drafts_json) do
+  #   Enum.each(payments(payments_drafts_json), &create_payment/1)  
+  # end
 end
---
+++ lib/financial_operations_elixir_web/services/payments/create.ex
@@ -0,0 +1,33 @@
+defmodule FinancialOperationsElixirWeb.Services.Payments.Create do
+  alias FinancialOperationsElixir.Payments
+  alias FinancialOperationsElixir.Payments.Payment
+  alias FinancialOperationsElixirWeb.Services.Utils.GenerateCode
+  
+  defmodule Payment do
+    defstruct tracking_code: "", value: 0.0, beneficiary_id: 0, transaction_id: 0, batch_id: 0 
+  end
+
+  defp payments(payments_drafts_json) do
+    Poison.decode!(payments_drafts_json, as: [%Payment{}])
+  end
+
+  defp payments_params(payment_draft) do
+    %{"tracking_code"=>payment_draft.tracking_code, "value"=>payment_draft.value, "transaction_id"=>payment_draft.transaction_id, "beneficiary_id"=>payment_draft.beneficiary_id, "batch_id"=>payment_draft.batch_id}
+  end
+
+  defp create_payment(payment_draft) do
+    tracking_code = GenerateCode.generate(10)
+    batch_id = System.unique_integer()
+    transaction_id = System.unique_integer()
+    beneficiary_id = System.unique_integer()
+    payment_draft = %{payment_draft | transaction_id: transaction_id, batch_id: batch_id, tracking_code: tracking_code, beneficiary_id: beneficiary_id}
+    
+    with {:ok, %FinancialOperationsElixir.Payments.Payment{} = payment} <- payments_params(payment_draft) |> Payments.create_payment() do
+      # create_transaction(payment)
+    end 
+  end 
+
+  def mount_payments(payments_drafts_json) do
+    Enum.each(payments(payments_drafts_json), &create_payment/1)  
+  end
+end
