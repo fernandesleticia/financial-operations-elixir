cb1e0bd372802e0b6542d9c8c82027144a65e453
fernandesleticia <leticiafernandes777@gmail.com>
2020-05-11 13:55:40 -0300

    feat: creating transaction id before payment

--- lib/financial_operations_elixir_web/services/payments/create.ex
+++ lib/financial_operations_elixir_web/services/payments/create.ex
@@ -16,16 +16,25 @@ defmodule FinancialOperationsElixirWeb.Services.Payments.Create do
     %{"tracking_code"=>payment_draft.tracking_code, "value"=>payment_draft.value, "transaction_id"=>payment_draft.transaction_id, "beneficiary_id"=>payment_draft.beneficiary_id, "batch_id"=>payment_draft.batch_id}
   end
 
-  defp create_payment(payment_draft, batch) do
+  defp create_payment(payment_draft, batch) do 
     tracking_code = GenerateCode.generate(10)
+
     batch_id = batch.id
-    transaction_id = System.unique_integer()
-    beneficiary_id = System.unique_integer()
+    beneficiary_id = System.unique_integer() # get beneficiary id from payment draft
+    transaction_draft = %{tracking_code: tracking_code, value: payment_draft["value"]}
+    transaction = create_transaction(transaction_draft)
+    transaction_id = transaction.id
+    
     payment_draft = %Payment{transaction_id: transaction_id, batch_id: batch_id, tracking_code: tracking_code, beneficiary_id: beneficiary_id}
+
     with {:ok, %FinancialOperationsElixir.Payments.Payment{} = payment} <- payments_params(payment_draft) |> Payments.create_payment() do
-      Create.create_transaction(payment)
+      IO.puts "PAYMENT CREATED"
     end 
   end 
+
+  defp create_transaction(payment_draft) do
+    Create.create_transaction(payment_draft)
+  end
 
   def mount_payments(payments_draft, batch) do
     Enum.each(payments_draft, &create_payment(&1, batch))  
--- lib/financial_operations_elixir_web/services/transactions/create.ex
+++ lib/financial_operations_elixir_web/services/transactions/create.ex
@@ -3,18 +3,18 @@ defmodule FinancialOperationsElixirWeb.Services.Transactions.Create do
   alias FinancialOperationsElixir.Transactions
   alias FinancialOperationsElixir.Transactions.Transaction
   
-  defp transactions_params(payment) do
-    final_amount = Exchange.cambio(payment.value, "USD") # get currebcy code from batch
+  defp transactions_params(transaction_draft) do
+    final_amount = Exchange.cambio(transaction_draft.value, "USD") # get currebcy code from batch
     currency_id = 1 #get currency id from batch
     payer_id = 1 #get payer id from batch
     account_id = 1 # get account id trougth beneficiary
     payer_value_date = ~D[2010-04-17] # get date from batch(add to batch)
-    %{"tracking_code"=>payment.tracking_code, "amount"=>payment.value, "final_amount"=>final_amount, "payer_value_date"=>payer_value_date, "currency_id"=>currency_id, "account_id"=>account_id, "payer_id"=>payer_id}
+    %{"tracking_code"=>transaction_draft.tracking_code, "amount"=>transaction_draft.value, "final_amount"=>final_amount, "payer_value_date"=>payer_value_date, "currency_id"=>currency_id, "account_id"=>account_id, "payer_id"=>payer_id}
   end
 
-  def create_transaction(payment) do
-    with {:ok, %FinancialOperationsElixir.Transactions.Transaction{} = transaction} <- transactions_params(payment) |> Transactions.create_transaction() do
-      IO.puts "|Transação criada|"
+  def create_transaction(transaction_draft) do
+    with {:ok, %FinancialOperationsElixir.Transactions.Transaction{} = transaction} <- transactions_params(transaction_draft) |> Transactions.create_transaction() do
+      transaction
     end 
   end
 end
